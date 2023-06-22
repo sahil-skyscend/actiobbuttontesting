f684b3527ac550544ec6e6a78d8692f8d6abf2cc
sahil-skyscend <tirkar.sahil222@gmail.com>
2023-06-22 13:05:53 +0530

    [ADD]deleted file for testing action button

--- sky_website/controllers/main.py
+++ sky_website/controllers/main.py
@@ -1,15 +1,1 @@
-from odoo import http
-from odoo.http import request
-
-
-class SkyWebsite(http.Controller):
-    @http.route(['/'], type='http', auth="public", website=True)
-    def sky_home_website(self, **kwargs):
-
-        """
-        This method will be used to display sky website
-        -----------------------------------------------
-        @param self: object pointer
-        @param kwargs: dictionary containing fields and values
-        """
-        return request.render("sky_website.home_sky_website_template")
+print('Testing Action Button')
